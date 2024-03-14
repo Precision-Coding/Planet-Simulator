bdfb4e4c9b637958d1315885e446041ca7731db0
Callum Lewis <callumlewis852@gmail.com>
2024-03-14 13:52:04 +0000

    Adds some resolution stuff and half way through scale my computer is buggin so gonna push just in case

--- main.py
+++ main.py
@@ -3,29 +3,51 @@ import pygame
 from sys import exit
 import math
 
-windowWidth, windowHeight = 1400, 700 # Size of actual window
-resolution = 1
+
+def planetCreate(radius, resolution):
+    radius = radius / resolution
+    planet = pygame.surface.Surface((radius * 3, radius * 3))
+    pygame.draw.circle(planet, "darkblue", (radius * 1.5, radius * 1.5), radius,)
+    planet_x, planet_y = 700, 350
+    planet_pos = planet.get_rect(center = (planet_x, planet_y))
+
+    return planet, planet_pos
+
+def cometCreate(radius, resolution, distance, size):
+    radius = radius * resolution
+    pixel_distance = distance / resolution
+    comet = pygame.surface.Surface((radius * 4, radius * 4))
+    pygame.draw.circle(comet, "darkblue", (radius * 2, radius * 2), radius,)
+    comet_x, comet_y = 0 / resolution + 700, 0 / resolution + 350
+    comet_pos = comet.get_rect(center = (comet_x, comet_y))
+
+    return comet, comet_pos
+
+def scaleCreate(resolution, window_width, font):
+    scale_bar = pygame.surface.Surface((window_width, 50))
+    scale_bar.fill((25, 25, 25))
+    for distance in range(1, 12):
+        pixel_space = window_width / 11
+        pygame.draw.line(scale_bar, "white", (distance * pixel_space, 0), (distance * pixel_space, 50))
+
+    text = font.render(f"{distance * pixel_space * resolution / 1000} Km", True, "white")
+    return scale_bar
+
+window_width, window_height = 1400, 700 # Size of actual window
+resolution = 100000
 
 # Framerate
-frameRate = 60
+frame_rate = 60
 clock = pygame.time.Clock() # I honestly don't know what this
 
 # Screen Setup
 pygame.init()
-screen = pygame.display.set_mode((windowWidth, windowHeight))
+screen = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Planet-Simulator")
 
 # Font
 pygame.font.init()
-baseFont = pygame.font.SysFont("helvetica", 20 * resolution)
-
-def planetCreate(radius, resolution):
-    radius = radius * resolution
-    planet = pygame.surface.Surface((radius * 4, radius * 4))
-    pygame.draw.circle(planet, "darkblue", (radius * 2, radius * 2), radius,)
-
-    return planet
-
+base_font = pygame.font.SysFont("helvetica", 20 * resolution)
 
 # Event Loop
 while True:
@@ -39,11 +61,12 @@ while True:
             resolution = resolution + event.y * resolution / 10
 
     # Surface Updates
-    planet = planetCreate(150, resolution)
+    planet, planet_pos = planetCreate(12000000, resolution)
+    scale_bar = scaleCreate(resolution, window_width, base_font)
 
-
-    screen.blit(planet, planet.get_rect(center = (700, 350)))
-
+    #Blits
+    screen.blit(planet, planet_pos)
+    screen.blit(scale_bar, (0, 0))
     # Updates and tickrate
     pygame.display.update()
-    clock.tick(frameRate)
+    clock.tick(frame_rate)
